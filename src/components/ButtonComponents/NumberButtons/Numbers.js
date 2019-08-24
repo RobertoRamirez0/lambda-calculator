86cb77b99b3fc7b79343f5c8ca95844712cd5835
RobertoRamirez0 <ramirez.robertomu22@gmail.com>
2019-08-24 19:03:34 -0400

    added evaluating function, calculator can now calculate. started styling elements

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import NumberButton from "./NumberButton"
 //Import your array data to from the provided data file
 import { numbers } from "../../../data";
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
   const [buttonNumbers, setButtonNumbers] = useState(numbers)
   return (
@@ -12,7 +12,7 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
-       {buttonNumbers.map(number => <NumberButton key={number} text={number}/>)}
+       {buttonNumbers.map(number => <NumberButton key={number} text={number} addNumber={props.addNumber}/>)}
     </div>
   );
 };
